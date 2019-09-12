0b743c99da6cf131590510283fa87e7bebac417f
Andrei Hincu <andreih1981@gmail.com>
2019-09-12 20:10:29 +0300

    init

---
+++ back/src/main/java/com/app/Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+   private static ConfigurableApplicationContext context;
+    public static void main(String[] args) {
+        context = SpringApplication.run(Application.class, args);
+    }
+}
