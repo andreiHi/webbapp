124341af39ac0773590a3385eb7e1edf7b85850c
Andrei Hincu <andreih1981@gmail.com>
2019-09-13 17:12:36 +0300

    Create structure

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,11 +2,13 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {DEBUG_INFO_ENABLED} from './app/app.constants';
 
-if (environment.production) {
+if (!DEBUG_INFO_ENABLED) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+
+platformBrowserDynamic().bootstrapModule(AppModule, {preserveWhitespaces: true})
+  .then((success) => console.log(`Application started`))
   .catch(err => console.error(err));
