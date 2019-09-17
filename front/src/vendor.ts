d5fad402b37737f35fedf71db80151bfa32b795f
Andrei Hincu <andreih1981@gmail.com>
2019-09-17 19:18:37 +0300

    add bootstrap

--- front/src/vendor.ts
+++ front/src/vendor.ts
@@ -4,6 +4,8 @@ import '@angular/common';
 import '@angular/core';
 import '@angular/forms';
 import '@angular/router';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
 
 // RxJS
 import 'rxjs';
